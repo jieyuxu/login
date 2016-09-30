cbd03130dd4cc92981b57a0222a3621476d79eb8
jieyuxu <jieyu.stuy@gmail.com>
2016-09-29 21:32:21 -0400

    added files

---
+++ login.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+usr = "Amy"
+password = "catscatscats"
+
+@app.route("/") #assign following fxn to run when root route requested
+@app.route("/login/")
+def table_this():
+	#print request.headers
+	return render_template('basicform.html', title="Login form")
+
+@app.route("/authenticate/", methods = ["POST"])
+def auth():
+	if request.form['user'] == usr and request.form['password'] == password:
+		return render_template("success.html")
+	return render_template("failure.html")
+    
+if __name__ == "__main__":
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
+
+
