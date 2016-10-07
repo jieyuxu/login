14f5ed9ac4eb92e03cfdc66d0683b2efff09753c
jieyuxu <jieyu.stuy@gmail.com>
2016-10-07 12:07:22 -0400

    updated login

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, session, render_template, request, url_for, redirect
 import csv 
 import hashlib
 
@@ -9,9 +9,13 @@ datab = csv.reader(storage)
 L = []
 D = {}
 
+app.secret_key = "MARSHMALLOWS"
+
 @app.route("/")
-@app.route("/login/", methods = ["POST", "GET"])
+@app.route("/login/", methods = ["POST"])
 def login():
+	if 'user' in session:
+		return redirect(url_for("welcome"))
 	return render_template('basicform.html', title="Login form")
 
 def toD(readin):
@@ -32,9 +36,10 @@ def auth():
 	D = toD(datab)
 	user = hashlib.sha512(request.form['user']).hexdigest()
 	passw = hashlib.sha512(request.form['password']).hexdigest()
+	session['user'] = request.form['user']
 	if user in D:
 		if D[user] == passw:
-			return render_template("status.html", message="SUCCESS YOU LOGGED IN")
+			return redirect(url_for("welcome"))
 	if request.form['submit'] == 'Register':
 		if user in D:
 			return render_template('basicform.html', title="Login form", message="Username taken")
@@ -42,10 +47,21 @@ def auth():
 			with open('users.csv', 'a') as file:
 				file.write(user+','+passw+"\n")
 		return render_template('basicform.html', title="Login form", message="Your account was created.")
-	return render_template("status.html", message="YOU FAILED TO LOGIN.")
+	return render_template("basicform.html", message="Login failed.")
+
+@app.route("/welcome/")
+def welcome():
+	return render_template("welcome.html", name= session['user'])
+
+@app.route("/logout/")
+def logout():
+	session.pop('user')
+	return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    #app.debug = True 
+    app.debug = True 
     app.run()
+
+
 
 
